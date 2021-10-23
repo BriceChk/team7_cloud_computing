8e37f9d5734030aa94dc639b1d993ecb8f2e5402
Joffrcy <colletjoffrey@live.fr>
2021-10-23 15:23:08 +0200

    Ajout des packages -Liste des utilisateurs en ligne -Affectation username au socket -Affichage connexion et deco avec username

--- app.js
+++ app.js
@@ -2,9 +2,33 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+let userList = [];
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
+app.get('/', (req, res) => {  res.sendFile(__dirname + '/index.html');});
+
+io.on('connection', (socket) => {
+
+    socket.on('user connected', (msg) => {
+        var username = msg;
+        socket.username = username;
+        userList.push(username);
+        io.emit('user connected', socket.username + " has joined the chat!");
+    });
+
+    socket.on('disconnect', () => {
+        userList = userList.filter(e => e !== socket.username);
+        io.emit("user disconnected", socket.username + " has left the chat!");
+    });
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('new username', (msg) => {
+        io.emit('new username', userList);
+    });
 });
 
 server.listen(3000, () => {
