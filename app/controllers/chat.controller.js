740efba0f1f39fd8cdc696da19f7b50347692ef3
BriceChk <brice.chkir@outlook.fr>
2021-12-02 02:07:27 +0100

    Removed useless stuff

--- app/controllers/chat.controller.js
+++ app/controllers/chat.controller.js
@@ -1,14 +1,6 @@
 const db = require("../models");
 const User = db.user;
 
-exports.test = (req, res) => {
-    res.status(200).send("Test ok.");
-};
-
-exports.testPublic = (req, res) => {
-    res.status(200).send("Test ok.");
-};
-
 exports.findUserById = (req, res) => {
     User.findOne({
         _id: req.query.id
